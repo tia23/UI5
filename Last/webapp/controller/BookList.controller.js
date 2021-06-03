3e985de7a1e4025383114b4ee43dfe3ac2d53650
tia23 <tia_23sb@yahoo.com>
2021-06-03 13:33:50 +0300

    final version

---
+++ Last/webapp/controller/BookList.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+], function (Controller) {
+    "use strict";
+    return Controller.extend("org.ubb.books.controller.BookList", {
+
+      
+    
+
+    });
+});
